9ffbe2926ceccc7066772eff500f6cd2063bda93
Kshitij Bhatnagar <bhatnagk@tcd.ie>
2017-10-16 18:13:31 +0100

    basic server

--- pyServer.py
+++ pyServer.py
@@ -1,1 +1,21 @@
 #Python Server for multiple clients
+
+import socket
+
+#creating socket object 
+s=socket.socket()
+host=socket.gethostname()
+print(host)
+port=12221
+
+s.bind((host,port))
+
+s.listen(7)
+
+while TRUE:
+    c,adrs=s.accept()
+    print("got connceted from %s", +str(adrs))
+    
+    response="welcome to the chatroom"
+    c.send(response.encode('ascii'))
+    c.close()
