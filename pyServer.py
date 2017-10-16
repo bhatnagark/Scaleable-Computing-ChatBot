4055dacdc9ae2819cf03db88533957f5f6c63747
Kshitij Bhatnagar <bhatnagk@tcd.ie>
2017-10-16 20:11:22 +0100

    Multi threaded server initialization

--- pyServer.py
+++ pyServer.py
@@ -1,6 +1,7 @@
 #Python Server for multiple clients
 
 import socket
+import threading
 
 #creating socket object 
 s=socket.socket()
@@ -8,12 +9,36 @@ host=socket.gethostname()
 print(host)
 port=12221
 
-s.bind((host,port))
+#defining chat server as threading thread
 
-s.listen(7)
+class ChatServer(threading.Thread):
+    #specifyying number of waiting clients
+    MAX_WAITING_CONNECTIONS=10
+    #Size of the message length
+    RECV_BUFFER=4096
+    #Placeholder for message length
+    RECV_MESG_LEN=10
+    
+    #Initialize new server
+    def init(self,host,port):
+        threading.thread.init(self)
+        self.host=host
+        self.port=port
+        self.running= TRUE #whether server should run or not
+    
+    #binds the multi threaded socket with host and port instead of previous single threaded socket    
+    def bind_socket(self):
+        
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4, continuos stream of data flow
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # socket option re use the address 
+        self.server_socket.bind((self.host, self.port)) # bind the host name and port
+        self.server_socket.listen(self.MAX_WAITING_CONNECTIONS) 
+        self.connections.append(self.server_socket)
+    
 
-while TRUE:
-    c,adrs=s.accept()
+#threading required
+while 1:
+    c,adrs= s.accept()
     print("got connceted from %s", +str(adrs))
     
     response="welcome to the chatroom"
