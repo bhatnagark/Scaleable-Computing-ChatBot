b9a3bcf37ca3002cfd8bdd20e08f7eed6558b90d
Kshitij Bhatnagar <bhatnagk@tcd.ie>
2017-11-01 18:06:28 +0000

    Join fucntion

--- pyServer.py
+++ pyServer.py
@@ -4,11 +4,6 @@ import socket
 import threading as Thread
 import random
 
-#creating socket object 
-s=socket.socket()
-host=socket.gethostname()
-print(host)
-port=12221
 
 #defining chat message 
 def check_msg(msg):
@@ -23,6 +18,34 @@ def check_msg(msg):
 		return(4)	
 	else:
 		return(5)
+    
+#Join function
+def join(conn_msg,csock):
+	print('Joiner')
+	gname = conn_msg.find(':'.encode('utf-8'))+2
+	gname_end = conn_msg.find('\n'.encode('utf-8'))-1
+	groupname = conn_msg[gname:gname_end]
+
+	cname = conn_msg.find('CLIENT_NAME'.encode('utf-8'))+13
+	cname_end = conn_msg.find(' '.encode('utf-8'),cname)
+	clientname = conn_msg[cname:cname_end]
+	rID = 0
+	
+	if (groupname.decode('utf-8')) == 'g1' :
+		g1_clients.append(clientname)
+		rID = 1001
+	elif groupname == 'g2' :
+		g2_clients.append(clientname)
+		rID = 1002
+	#sending ackowledgement
+	response = "JOINED_CHATROOM: ".encode('utf-8') + groupname+ "\n".encode('utf-8')
+	response += "SERVER_IP: \n".encode('utf-8')
+	response += "PORT: \n".encode('utf-8')
+	response += "ROOM_REF: ".encode('utf-8') + str(rID).encode('utf-8') +'\n'.encode('utf-8')
+	response += "JOIN_ID: ".encode('utf-8') + str(clThread.uid).encode('utf-8')   
+
+	csock.send(response)
+	return groupname,clientname    
 
 #diconnect function
 def discon():
